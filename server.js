c0f98ac77b7eaf4c2970f79fc9b2a51c03a2e568
Joel Myrtil <myrtil.joel@gmail.com>
2023-04-30 10:09:16 -0700

    server edit

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 'use strict'
 
 require('dotenv').config();
-const handleError = require('./errorhandlers/500');
-const handleNotFound = require('./errorhandlers/404');
+const handleError = require('./src/errorhandlers/500');
+const handleNotFound = require('./src/errorhandlers/400');
+const weatherAPI = require('./src/middleware/weatherapi')
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res, next) => {
   res.send('Server Live');
@@ -15,6 +17,7 @@ app.get('/', (req, res, next) => {
 
 app.use('*', handleNotFound);
 app.use(handleError);
+app.get('/weather', weatherAPI);
 
 module.exports = {
   app,
---
+++ src/middleware/weatherapi.js
@@ -0,0 +1,27 @@
+'use strict';
+const axios = require('axios');
+
+
+class Weather {
+  constructor(apiForcast) {
+    this.humidity = apiForcast.rh;
+    this.temp = apiForcast.temp;
+    this.windspeed = apiForcast.wind_spd
+  }
+}
+
+//can city be used to request data from weatherbit api instead of lat lon?
+async function weatherAPI (request, response, next) {
+  try {
+    let lat = request.query.lat;
+    let lon = request.query.lon;
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.API_KEY}&lat=${lat}&lon=${lon}&units=I&days=7`;
+    let apiForcast = await axios(url);
+    let apiWeather = apiForcast.data.data.map(temp => new Weather (temp));
+    response.send(apiWeather);
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = weatherAPI;
