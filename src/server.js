6b050e0c3ef0a39a17c58bc09c338ef1bd2a8d6f
Joel Myrtil <myrtil.joel@gmail.com>
2023-05-01 02:10:40 -0700

    mongo crud

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const handleError = require('./errorhandlers/500');
 const handleNotFound = require('./errorhandlers/400');
 const getCity = require('./getCityWeather')
+const Data = require('./models/mongoweather')
 const express = require('express');
 const cors = require('cors');
 // const prompt = require('prompt-sync')();
@@ -15,6 +16,10 @@ app.use(express.json());
 app.get('/', (req, res, next) => {
   res.send('Server Live');
 });
+
+app.post(Data.addWeather);
+app.delete(Data.deleteWeather);
+app.put(Data.updateWeather);
 
 setTimeout(() => {
  getCity();
