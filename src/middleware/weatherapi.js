94bfff393137698506cff8c78e6294b5062156b3
Miranda Lu <121185796+mirandalu2020@users.noreply.github.com>
2023-05-01 16:09:26 -0700

    Update weatherapi.js

--- src/middleware/weatherapi.js
+++ src/middleware/weatherapi.js
@@ -1,27 +1,52 @@
 'use strict';
+
 const axios = require('axios');
+const prompt = require('prompt-sync')();
+const readline = require("readline");
+const rl =
+ readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
+const { cache } = require('./cache');
 
-class Weather {
-  constructor(apiForcast) {
-    this.humidity = apiForcast.rh;
-    this.temp = apiForcast.temp;
-    this.windspeed = apiForcast.wind_spd
+async function getWeeklyWeather(req, res, next){
+  //https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=API_KEY
+  let weatherData = {
+    humidity: '',
+    maxTemperature: '',
+    minTemperature: '',
+    windspeed: '',
+    feelsLikeMax: '',
+    feelsLikeMin:'',
   }
+  let config = {
+    baseURL: 'http://api.weatherbit.io/v2.0/forecast/daily',
+    params: {
+      key:process.env.WEATHERBIT_API_KEY,
+      city: 'Seattle',
+      days: 5,
+    },
+    method:'get',
+  }
+
+  try{
+      let apiForecast = await axios(config);
+      console.log(apiForecast.data)
+      weatherData['humidity'] = apiForecast.data.data[0].rh;
+      weatherData['windspeed'] = apiForecast.data.data[0].wind_spd;
+      weatherData['maxTemperature'] = apiForecast.data.data[0].max_temp;
+      weatherData['minTemperature'] = apiForecast.data.data[0].min_temp;
+      weatherData['feelsLikeMax'] = apiForecast.data.data[0].app_max_temp;
+      weatherData['feelsLikeMin'] = apiForecast.data.data[0].app_min_temp;
+
+  } catch (err){
+    console.log(err)
+    };
+
+  console.log(weatherData);
+  return weatherData;
 }
 
-//can city be used to request data from weatherbit api instead of lat lon?
-async function weatherAPI (request, response, next) {
-  try {
-    let lat = request.query.lat;
-    let lon = request.query.lon;
-    let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.API_KEY}&lat=${lat}&lon=${lon}&units=I&days=7`;
-    let apiForcast = await axios(url);
-    let apiWeather = apiForcast.data.data.map(temp => new Weather (temp));
-    response.send(apiWeather);
-  } catch (error) {
-    next(error);
-  }
-}
-
-module.exports = weatherAPI;
+module.exports = getWeeklyWeather;
