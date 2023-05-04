768cccd704bacf524ef4c437e4eba2863c60fd33
mirandalu2020 <yuanling1007@hotmail.com>
2023-05-04 15:00:42 -0700

    send email test from kawika added

--- src/database/model.js
+++ src/database/model.js
@@ -5,6 +5,9 @@ const mongoose = require ('mongoose');
 const recommendationSchema = new mongoose.Schema({
   email: String,
   message: String,
-})
+});
+
+
+
 
 module.exports = mongoose.model('User Recommendations', recommendationSchema)
--- src/email/sendEmail.js
+++ src/email/sendEmail.js
@@ -6,52 +6,52 @@ const { SESv2Client, SendEmailCommand } = require("@aws-sdk/client-sesv2");
 
 const client = new SESv2Client({ region: "us-west-2" });
 
-const sendEmail = async (emailBody, title ='Clueless Caterpillar\s Recommendation') => {
-const input = { // SendEmailRequest
-  FromEmailAddress: process.env.AWS_EMAIL_ADDRESS,
-  FromEmailAddressIdentityArn: process.env.AWS_EMAIL_ARN,
-  Destination: { // Destination
-    ToAddresses: [ // EmailAddressList
-      // "kmillerartwork@gmail.com",
-      process.env.TEST_EMAIL
+const sendEmail = async (reccomendation) => {
+
+  const input = { // SendEmailRequest
+    FromEmailAddress: process.env.AWS_EMAIL_ADDRESS,
+    FromEmailAddressIdentityArn: process.env.AWS_EMAIL_ARN,
+    Destination: { // Destination
+      ToAddresses: [ // EmailAddressList
+        "kmillerartwork@gmail.com",
+      ],
+    },
+    ReplyToAddresses: [
+      process.env.AWS_EMAIL_ADDRESS,
     ],
-    // CcAddresses: [
-    //   "STRING_VALUE",
-    // ],
-    // BccAddresses: [
-    //   "STRING_VALUE",
-    // ],
-  },
-  ReplyToAddresses: [
-    process.env.AWS_EMAIL_ADDRESS,
-  ],
-  FeedbackForwardingEmailAddress: process.env.AWS_EMAIL_ADDRESS,
-  FeedbackForwardingEmailAddressIdentityArn: process.env.AWS_EMAIL_ARN,
-  Content: { // EmailContent
-    Simple: { // Message
-      Subject: { // Content
-        Data: title, // required
-        Charset: "utf-8",
-      },
-      Body: { // Body
-        Text: {
-          Data: emailBody, // required
+    FeedbackForwardingEmailAddress: process.env.AWS_EMAIL_ADDRESS,
+    FeedbackForwardingEmailAddressIdentityArn: process.env.AWS_EMAIL_ARN,
+    Content: { // EmailContent
+      Simple: { // Message
+        Subject: { // Content
+          Data: "Your Clothing Recommendation from Clueless Caterpillar!", // required
           Charset: "utf-8",
+        },
+        Body: { // Body
+          Text: {
+            Data: reccomendation, // required
+            Charset: "utf-8",
+          },
         },
       },
     },
-  },
-};
-
+  };
+  
   const command = new SendEmailCommand(input);
+
   try {
     const response = await client.send(command);
-    console.log(response);
+    console.log(`Email successfully sent: `, reccomendation);
   }
   catch (err) {
-    console.log(err);
+    console.log('ERROR SENDING EMAIL: ', err)
+    throw new Error('Could not send email')
   }
 }
 
-module.exports = sendEmail;
+// sendEmail();
 
+module.exports = {
+  sendEmail,
+  client
+}
